c64cac4a4ef3f9ddfe20b7715672db72c2b3c8be
Franco Zanardi <francozanardi.97@gmail.com>
2025-07-30 17:07:37 -0300

    fix: row/column paint bounds should use children real position to get right bounds.
    Keep in mind that intrinsic content bounds do not need to use real positions since they are only used if size() is fit-content, that is, when we don't care about the children positions.

--- src/pictex/nodes/column_node.py
+++ src/pictex/nodes/column_node.py
@@ -1,15 +1,10 @@
-from typing import Tuple
+from typing import Tuple, Callable
 from .node import Node
-from ..painters import Painter, BackgroundPainter, BorderPainter
-from ..models import Style, HorizontalAlignment, VerticalDistribution
+from .container_node import ContainerNode
+from ..models import HorizontalAlignment, VerticalDistribution
 import skia
 
-class ColumnNode(Node):
-
-    def __init__(self, style: Style, children: list[Node]) -> None:
-        super().__init__(style)
-        self._set_children(children)
-        self.clear()
+class ColumnNode(ContainerNode):
 
     def _compute_intrinsic_content_bounds(self) -> skia.Rect:
         visible_children = self._get_visible_children()
@@ -23,38 +18,17 @@ class ColumnNode(Node):
         max_child_width = max(child.layout_bounds.width() for child in visible_children)
         return skia.Rect.MakeWH(max_child_width, total_intrinsic_height)
 
-    def _compute_paint_bounds(self) -> skia.Rect:
-        paint_bounds = skia.Rect.MakeEmpty()
-
-        for child in self.children:
-            if child.computed_styles.position.get() is not None:
-                continue
-
-            child_bounds_shifted = child.paint_bounds.makeOffset(0, paint_bounds.height())
-            paint_bounds.join(child_bounds_shifted)
-
-        paint_bounds.join(self._compute_shadow_bounds(self.box_bounds, self.computed_styles.box_shadows.get()))
-        paint_bounds.join(self.layout_bounds)
-        return paint_bounds
-
-    def _get_painters(self) -> list[Painter]:
-        return [
-            BackgroundPainter(self.computed_styles, self.box_bounds, self._render_props.is_svg),
-            BorderPainter(self.computed_styles, self.box_bounds),
-        ]
-
-    def _set_absolute_position(self, x: float, y: float) -> None:
-        self._absolute_position = (x, y)
-
+    def _calculate_children_relative_positions(self, children: list[Node], get_child_bounds: Callable[[Node], skia.Rect]) -> list[Tuple[float, float]]:
+        positions = []
         user_gap = self.computed_styles.gap.get()
         alignment = self.computed_styles.horizontal_alignment.get()
-        visible_children = [child for child in self.children if child.computed_styles.position.get() is None]
-        start_y, distribution_gap = self._distribute_vertically(user_gap, visible_children)
+        start_y, distribution_gap = self._distribute_vertically(user_gap, children)
 
         final_gap = user_gap + distribution_gap
         current_y = start_y
-        for child in visible_children:
-            child_width = child.layout_bounds.width()
+        for child in children:
+            child_bounds = get_child_bounds(child)
+            child_width = child_bounds.width()
             container_width = self.content_bounds.width()
             child_x = self.content_bounds.left()
 
@@ -63,14 +37,16 @@ class ColumnNode(Node):
             elif alignment == HorizontalAlignment.RIGHT:
                 child_x += container_width - child_width
 
-            child._set_absolute_position(x + child_x, y + current_y)
-            current_y += child.layout_bounds.height() + final_gap
+            positions.append((child_x, current_y))
+            current_y += child_bounds.height() + final_gap
 
-    def _distribute_vertically(self, user_gap: float, visible_children: list[Node]) -> Tuple[float, float]:
+        return positions
+
+    def _distribute_vertically(self, user_gap: float, children: list[Node]) -> Tuple[float, float]:
         distribution = self.computed_styles.vertical_distribution.get()
         container_height = self.content_bounds.height()
-        children_total_height = sum(child.layout_bounds.height() for child in visible_children)
-        total_gap_space = user_gap * (len(visible_children) - 1)
+        children_total_height = sum(child.layout_bounds.height() for child in children)
+        total_gap_space = user_gap * (len(children) - 1)
         extra_space = container_height - children_total_height - total_gap_space
 
         start_y = self.content_bounds.top()
@@ -79,13 +55,13 @@ class ColumnNode(Node):
             start_y += extra_space
         elif distribution == VerticalDistribution.CENTER:
             start_y += extra_space / 2
-        elif distribution == VerticalDistribution.SPACE_BETWEEN and len(visible_children) > 1:
-            distribution_gap = extra_space / (len(visible_children) - 1)
+        elif distribution == VerticalDistribution.SPACE_BETWEEN and len(children) > 1:
+            distribution_gap = extra_space / (len(children) - 1)
         elif distribution == VerticalDistribution.SPACE_AROUND:
-            distribution_gap = extra_space / len(visible_children)
+            distribution_gap = extra_space / len(children)
             start_y += distribution_gap / 2
         elif distribution == VerticalDistribution.SPACE_EVENLY:
-            distribution_gap = extra_space / (len(visible_children) + 1)
+            distribution_gap = extra_space / (len(children) + 1)
             start_y += distribution_gap
 
         return start_y, distribution_gap
--- src/pictex/nodes/row_node.py
+++ src/pictex/nodes/row_node.py
@@ -1,15 +1,10 @@
-from typing import Tuple
+from typing import Tuple, Callable
+from .container_node import ContainerNode
 from .node import Node
-from ..painters import Painter, BackgroundPainter, BorderPainter
-from ..models import Style, VerticalAlignment, HorizontalDistribution
+from ..models import VerticalAlignment, HorizontalDistribution
 import skia
 
-class RowNode(Node):
-
-    def __init__(self, style: Style, children: list[Node]) -> None:
-        super().__init__(style)
-        self._set_children(children)
-        self.clear()
+class RowNode(ContainerNode):
 
     def _compute_intrinsic_content_bounds(self) -> skia.Rect:
         visible_children = self._get_visible_children()
@@ -23,38 +18,17 @@ class RowNode(Node):
         max_child_height = max(child.layout_bounds.height() for child in visible_children)
         return skia.Rect.MakeWH(total_intrinsic_width, max_child_height)
 
-    def _compute_paint_bounds(self) -> skia.Rect:
-        paint_bounds = skia.Rect.MakeEmpty()
-
-        for child in self.children:
-            if child.computed_styles.position.get() is not None:
-                continue
-
-            child_bounds_shifted = child.paint_bounds.makeOffset(paint_bounds.width(), 0)
-            paint_bounds.join(child_bounds_shifted)
-
-        paint_bounds.join(self._compute_shadow_bounds(self.box_bounds, self.computed_styles.box_shadows.get()))
-        paint_bounds.join(self.layout_bounds)
-        return paint_bounds
-
-    def _get_painters(self) -> list[Painter]:
-        return [
-            BackgroundPainter(self.computed_styles, self.box_bounds, self._render_props.is_svg),
-            BorderPainter(self.computed_styles, self.box_bounds),
-        ]
-
-    def _set_absolute_position(self, x: float, y: float) -> None:
-        self._absolute_position = (x, y)
-
+    def _calculate_children_relative_positions(self, children: list[Node], get_child_bounds: Callable[[Node], skia.Rect]) -> list[Tuple[float, float]]:
+        positions = []
         alignment = self.computed_styles.vertical_alignment.get()
         user_gap = self.computed_styles.gap.get()
-        visible_children = self._get_visible_children()
-        distribution_gap, start_x = self._distribute_horizontally(user_gap, visible_children)
+        distribution_gap, start_x = self._distribute_horizontally(user_gap, children)
 
         final_gap = user_gap + distribution_gap
         current_x = start_x
-        for child in visible_children:
-            child_height = child.layout_bounds.height()
+        for child in children:
+            child_bounds = get_child_bounds(child)
+            child_height = child_bounds.height()
             container_height = self.content_bounds.height()
             child_y = self.content_bounds.top()
 
@@ -63,14 +37,16 @@ class RowNode(Node):
             elif alignment == VerticalAlignment.BOTTOM:
                 child_y += container_height - child_height
 
-            child._set_absolute_position(x + current_x, y + child_y)
-            current_x += child.layout_bounds.width() + final_gap
+            positions.append((current_x, child_y))
+            current_x += child_bounds.width() + final_gap
 
-    def _distribute_horizontally(self, user_gap: float, visible_children: list[Node]) -> Tuple[float, float]:
+        return positions
+
+    def _distribute_horizontally(self, user_gap: float, children: list[Node]) -> Tuple[float, float]:
         distribution = self.computed_styles.horizontal_distribution.get()
         container_width = self.content_bounds.width()
-        children_total_width = sum(child.layout_bounds.width() for child in visible_children)
-        total_gap_space = user_gap * (len(visible_children) - 1)
+        children_total_width = sum(child.layout_bounds.width() for child in children)
+        total_gap_space = user_gap * (len(children) - 1)
         extra_space = container_width - children_total_width - total_gap_space
 
         start_x = self.content_bounds.left()
@@ -79,13 +55,13 @@ class RowNode(Node):
             start_x += extra_space
         elif distribution == HorizontalDistribution.CENTER:
             start_x += extra_space / 2
-        elif distribution == HorizontalDistribution.SPACE_BETWEEN and len(visible_children) > 1:
-            distribution_gap = extra_space / (len(visible_children) - 1)
+        elif distribution == HorizontalDistribution.SPACE_BETWEEN and len(children) > 1:
+            distribution_gap = extra_space / (len(children) - 1)
         elif distribution == HorizontalDistribution.SPACE_AROUND:
-            distribution_gap = extra_space / len(visible_children)
+            distribution_gap = extra_space / len(children)
             start_x += distribution_gap / 2
         elif distribution == HorizontalDistribution.SPACE_EVENLY:
-            distribution_gap = extra_space / (len(visible_children) + 1)
+            distribution_gap = extra_space / (len(children) + 1)
             start_x += distribution_gap
 
         return distribution_gap, start_x
