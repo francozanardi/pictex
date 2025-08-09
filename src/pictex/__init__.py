528ce3dce5a227b422d08453dfe3ee184fb98bb0
Franco Zanardi <francozanardi.97@gmail.com>
2025-08-09 20:22:56 -0300

    chore: release 1.1.1 version

--- src/pictex/__init__.py
+++ src/pictex/__init__.py
@@ -7,7 +7,7 @@ from .models.public import *
 from .bitmap_image import BitmapImage
 from .vector_image import VectorImage
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 __all__ = [
     "Canvas",
